3ff319e9e7a99cce8890064872b93d6f086616f3
thejsgirl <prinegi100@gmail.com>
2019-06-24 12:36:01 +0530

    feat: redux dummy setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './rootReducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {};
+const store = createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/rootReducers/index.js
@@ -0,0 +1,7 @@
+
+import { combineReducers } from 'redux';
+import data from '../reducers';
+
+export default combineReducers({
+  data,
+});
